600493de393ba794c6345cf480d0ee22cba353c8
jaechu12 <145534020+jaechu12@users.noreply.github.com>
2023-12-28 23:19:43 +0900

    Update app2.js

--- app2.js
+++ app2.js
@@ -44,7 +44,7 @@ app.post('/progress', async (req, res) => {
 
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'clothes.html'));
+  res.sendFile(path.join(__dirname, 'public', 'ot.html'));
 });
 
 
