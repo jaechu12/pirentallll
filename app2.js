714c4215d7c782d45af2c574d286adccacf836ac
jaechu12 <145534020+jaechu12@users.noreply.github.com>
2023-12-28 23:12:36 +0900

    Update app2.js

--- app2.js
+++ app2.js
@@ -1,4 +1,3 @@
-// server.js
 
 const express = require('express');
 const mongoose = require('mongoose');
@@ -7,20 +6,18 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// MongoDB 연결 설정
 mongoose.connect('mongodb://localhost:27017/progressDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
-// MongoDB 모델 정의
+
 const Progress = mongoose.model('Progress', {
   studentId: String,
   progress: Number,
 });
 
-// 미들웨어 설정
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public'))); // 정적 파일 제공
+app.use(express.static(path.join(__dirname, 'public'))); 
 
-// 진행도 조회 API
+
 app.get('/progress/:studentId', async (req, res) => {
   const studentId = req.params.studentId;
 
@@ -33,7 +30,6 @@ app.get('/progress/:studentId', async (req, res) => {
   }
 });
 
-// 진행도 업데이트 API
 app.post('/progress', async (req, res) => {
   const { studentId, progress } = req.body;
 
@@ -46,12 +42,12 @@ app.post('/progress', async (req, res) => {
   }
 });
 
-// 루트 경로에 대한 응답 추가 (index.html 제공)
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'clothes.html'));
 });
 
-// 서버 시작
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
